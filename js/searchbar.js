56116539c7b344bce9044035c643859928d33f23
brr <skibidibada@users.noreply.github.com>
2025-04-28 21:08:12 +0200

    Connect the searchbar function with a json file

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,15 +1,57 @@
-document.getElementById("searchbar_UL").style.display = "none";
+document.getElementById("searchbar_section").style.display = "none";
+
+let websites = [];
+const websitesContainer = document.getElementById("websites-container");
+
+async function loadSites() {
+  const response = await fetch("json/website_list_searchbar.json");
+  const data = await response.json();
+  websites = data;
+  renderWebsites();
+}
+
+function renderWebsites() {
+  websitesContainer.innerHTML = "";
+
+  for (let website of websites) {
+    const websiteElement = createWebsiteElement(website);
+    websitesContainer.appendChild(websiteElement);
+  }
+}
+
+function createWebsiteElement(website) {
+  const a = document.createElement("a");
+  const figure = document.createElement("figure");
+  a.classList.add("website");
+  a.classList.add("ae_rg");
+  a.classList.add("searchlist");
+
+  figure.setAttribute("data-id", website.id);
+
+  a.setAttribute("href", website.link);
+
+  const title = document.createElement("strong");
+  title.innerText = website.name;
+
+  const caption = document.createElement("figcaption");
+  caption.appendChild(title);
+  figure.appendChild(caption);
+  a.appendChild(figure);
+
+  return a;
+}
+loadSites();
 
 function myFunction() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("searchbar_Input");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("searchbar_UL");
-  li = ul.getElementsByClassName("searchlist");
+  section = document.getElementById("searchbar_section");
+  li = section.getElementsByClassName("searchlist");
 
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+    figcaption = li[i].getElementsByTagName("figcaption")[0];
+    txtValue = figcaption.textContent || figcaption.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "block";
     } else {
@@ -17,9 +59,9 @@ function myFunction() {
     }
 
     if (filter.length > 0) {
-      document.getElementById("searchbar_UL").style.display = "block";
+      document.getElementById("searchbar_section").style.display = "block";
     } else {
-      document.getElementById("searchbar_UL").style.display = "none";
+      document.getElementById("searchbar_section").style.display = "none";
     }
   }
 }
